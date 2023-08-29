cecd09f5f3c0bb9b2f346982667fdfa1baa90dba
SinthiyaBalan <sinthiyamct@gmail.com>
2023-08-29 19:46:30 +0200

    Updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import Home from './components/Home'
 
 function App() {
   
   return (
     <>
-    
+    <Nav/>
+    <Home/>
+    <Footer/>
     </>
   )
 }
