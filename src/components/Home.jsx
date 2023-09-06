0fa3f01658b7bc3eaf3026510f45700923735031
NithyadeviChandran <nithyadevi.ch@gmail.com>
2023-09-06 22:06:38 +0200

    addded the explore our paradise images and paragraphs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import heroImage from "/src/assets/images/hero3.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretLeft, faCaretRight, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import Winery from '/src/assets/images/Winery.png'
+import Beach from '/src/assets/images/Beach.png'
+import Nature from '/src/assets/images/Nature.png'
+import Restaurant from '/src/assets/images/Restaurant.png'
+import Santuranio from '/src/assets/images/Santuranio.png'
+import Sport from '/src/assets/images/Sport.png'
+
+
 
 function Home() {
   return (
@@ -22,21 +32,70 @@ function Home() {
       </div>
 
       <div>
-        <h2>Welcome to Casa Ripa</h2>
-        <h4>Holiday Home @ Cupramontana, Le Marche, Italy</h4>
-        <p>
+        <h2 className="m-4 font-bold">Welcome to Casa Ripa</h2>
+        <h4 className="font-custom text-justify m-2">Holiday Home @ Cupramontana, Le Marche, Italy</h4>
+        <p className="font-custom text-justify p-4">
           In the heart of Le Marche region our recently renovated and restored
           farmhouse sits just below the Hilltop of Cupramontana. From here you
           can relax and take in the breathtaking and panoramic views over the
-          surrounding countryside. The region of Le Marche is a beautiful hilly
-          and mountainous region and still relatively undiscovered by tourists,
-          but yet it is only 30 minutes away from Ancona Airport. Here you can
-          spend your holiday without stress and enjoy everything the region has
-          to offer, from the wonderful food and famous Verdicchio wine, the
-          warmth and friendliness of the people, the culture, nature and
-          fabulous blue-flag beaches along the Adriatic Coast. We hope to
-          welcome you to this paradise!
+          surrounding countryside. 
+          <button className="btn border border-gray-500 rounded sm:btn-sm md:btn-md lg:btn-lg">More<FontAwesomeIcon icon={faCaretRight} /></button>
+          </p>
+          <p className="font-custom text-justify m-2">
+          We hope to welcome you to this paradise!
         </p>
+        <h2 className="m-4 font-bold">ENJOY OUR PARADISE</h2>
+        <div className="flex justify-between flex-col lg:flex-row">
+        <div>
+        <img src={Winery} alt="Casa ripa winery image" className="w-72 m-4 rounded-lg shadow-lg mb-4"/>
+       <h2 className="m-4 text-lg font-bold">Winery</h2>
+        <p className="font-custom w-72 text-justify p-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Nulla porttitor accumsan tincidunt.
+            Proin sagittis nisl in tortor hendrerit tincidunt.<button className="btn border border-gray-500 rounded sm:btn-sm md:btn-md lg:btn-lg">More<FontAwesomeIcon icon={faCaretRight} /></button>
+        </p>
+        </div>
+        <div>
+        <img src={Beach} alt="Casa ripa beach image" className="w-72 m-4 rounded-lg shadow-lg mb-4"/>
+        <h2 className="m-4 text-lg font-bold">Beach</h2>
+        <p className="font-custom w-72 text-justify p-4">
+        The Adriatic coast is about 35km away and there are several beaches with umbrellas and sunbeds.<button className="btn border border-gray-500 rounded sm:btn-sm md:btn-md lg:btn-lg">More<FontAwesomeIcon icon={faCaretRight} /></button>
+        </p>
+        </div>
+        <div>
+        <img src={Sport} alt="Casa ripa sport image" className="w-72 m-4 rounded-lg shadow-lg mb-4"/>
+        <h2 className="m-4 text-lg font-bold">Sport and Activities</h2>
+        <p className="font-custom w-72 text-justify p-4">
+        Discover the range of sports available to help you relax and keep fit around CASA RIPA<button className="btn border border-gray-500 rounded sm:btn-sm md:btn-md lg:btn-lg">More<FontAwesomeIcon icon={faCaretRight} /></button>
+        </p>
+        </div>
+        </div>
+        <div className="flex justify-between flex-col lg:flex-row">
+        <div>
+        <img src={Santuranio} alt="Casa ripa santuranio image" className="w-72 m-4 rounded-lg shadow-lg mb-4"/>
+        <h2 className="m-4 text-lg font-bold">Santuario Madonna di Frasassi</h2>
+        <p className="font-custom w-72 text-justify p-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor accumsan 
+            tincidunt. Proin sagittis nisl in tortor hendrerit tincidunt.<button className="btn border border-gray-500 rounded sm:btn-sm md:btn-md lg:btn-lg">More<FontAwesomeIcon icon={faCaretRight} /></button>
+        </p>
+        </div>
+        <div>
+        <img src={Nature} alt="Casa ripa nature image" className="w-72 m-4 rounded-lg shadow-lg mb-4"/>
+        <h2 className="m-4 text-lg font-bold">Near by Cupramontana</h2>
+        <p className="font-custom w-72 text-justify p-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor accumsan tincidunt. Proin sagittis nisl in tortor hendrerit tincidunt.<button className="btn border border-gray-500 rounded sm:btn-sm md:btn-md lg:btn-lg">More<FontAwesomeIcon icon={faCaretRight} /></button>
+        </p>
+        </div>
+        <div>
+        <img src={Restaurant} alt="Casa ripa restaurant image" className="w-72 m-4 rounded-lg shadow-lg mb-4"/>
+        <h2 className="m-4 text-lg font-bold">Restaurants and Shops</h2>
+        <p className="font-custom w-72 text-justify p-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor accumsan tincidunt. Proin sagittis nisl in tortor hendrerit tincidunt.
+            <button className="btn border border-gray-500 rounded sm:btn-sm md:btn-md lg:btn-lg">More<FontAwesomeIcon icon={faCaretRight} /></button>
+        </p>
+        </div>
+        </div>
+
       </div>
     </div>
   );
