379a07ecf74c9e74de3dac8f6526f73809eb697b
NithyadeviChandran <nithyadevi.ch@gmail.com>
2023-11-21 20:20:41 +0100

    added the google calendar for Room1 and Room2

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -11,12 +11,17 @@ function Room({ room, fromdate, todate }) {
     <>
       <div className="conatiner">
         <div className="ml-36 mb-4 sm:flex flex-col lg:flex justify-center  gap-2 border-solid border-2 border-white-600">
-          <div className="div">
+          <div className="div lg:flex">
             <img
               src={room.imageurls[0]}
               style={{ width: 500, height: 300, padding: 10 }}
             />
-
+                    <div className="div">
+                <iframe
+        className="calendar"  style={{ width: 500, height: 300, padding: 10 }}
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FBerlin&hl=en_GB&showTz=0&showCalendars=0&showTabs=0&showTitle=1&src=ZDcwNThkOWQzODM5YzczZDYzNTM1Y2IxY2U5MDE4OWMwNjA2NTMzNmYyZjAzYjU0NzE5ZjAxMGI2NTVkMGYwYUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23EF6C00"
+      ></iframe> 
+      </div>
             <div className="m-4 sm:flex flex-col lg: flex flex-row gap-2">
               <h1>Name: {room.Name}</h1>
               <p>Description: {room.Description}</p>
@@ -30,10 +35,14 @@ function Room({ room, fromdate, todate }) {
                 <Button className="btn btn-primary m-2" onClick={handleShow}>
                   View Details
                 </Button>
+        
               </div>
+            
+              
             </div>
           </div>
         </div>
+        
       </div>
 
       {/* Modal to display room images */}
